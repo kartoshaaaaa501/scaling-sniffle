4678dfbd84f2547d7f8ebb955c17543b947f68c4
Igor <s26e_gritsenko@179.ru>
2023-10-26 14:44:41 +0300

    frontend

--- frontend.py
+++ frontend.py
@@ -1,13 +1,23 @@
 from cod import generator
 name = input("your name\n")
 print (f" Hello, {name}")
-
 print (f" How many examples would you like?" )
-n = int (input())
+while True:
+    try:
+        n = int(input())
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 print (f"сколько чисел в примере?")
-ur = int(input())
+while True:
+    try:
+        ur = int(input(""))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
 
 tasks = generator(n, ur)
+qwerty = 0
 x = 0
 while x < len(tasks):
         a = tasks[x]
@@ -15,6 +25,19 @@ while x < len(tasks):
         otvet = int(input())
         if otvet == eval(a):
                 print ("greaet job")
+                qwerty += 1
         else:
+
                 print ("not great job")
+                print (f"cool answer is: {eval(a)}")
         x += 1
+if qwerty == 0:
+    print ("тебе нужно очень сильно подтянуть математику..")
+elif 0.3 <= qwerty / n <= 0.4:
+    print  ("треть правильных ответов. подтяни математику")
+elif qwerty / n < 0.3:
+    print ("меньше треети правильных ответов. Я сейчас ИЕ позову...")
+elif 0.4 < qwerty / n <= 0.6:
+    print ("примерно половина. не очень как-то")
+elif qwerty / n > 0.6:
+    print ("бог математики")
