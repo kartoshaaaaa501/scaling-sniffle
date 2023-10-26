7dd1cf393bded1dcb9f6c66aec2baa8e9cb3005f
Igor <s26e_gritsenko@179.ru>
2023-10-26 13:35:05 +0300

    frontend

--- frontend.py
+++ frontend.py
@@ -1,30 +1,20 @@
-import random
 from cod import generator
-
-#ef generator(n):
-#       l = []
-#       for i in range(n):
-#           l.append(f"{random.randint(0, 10)} + {random.randint(0, 10)}")
-#       return l
-
-
 name = input("your name\n")
-print (f" Hello, {name}") 
+print (f" Hello, {name}")
 
 print (f" How many examples would you like?" )
 n = int (input())
-print (f"какой уровень?")
+print (f"сколько чисел в примере?")
 ur = int(input())
 
 tasks = generator(n, ur)
 x = 0
 while x < len(tasks):
-	a = tasks[x]
-	print (a)
-	otvet = int(input())
-	if otvet == eval(a):
-		print ("greaet job")
-	else:
-		print ("not great job")
-	x += 1
-
+        a = tasks[x]
+        print (a)
+        otvet = int(input())
+        if otvet == eval(a):
+                print ("greaet job")
+        else:
+                print ("not great job")
+        x += 1
