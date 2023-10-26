0047171668cf21503cba87c7e503eb53e05144bf
Igor <s26e_gritsenko@179.ru>
2023-10-26 14:45:59 +0300

    backend

--- cod.py
+++ cod.py
@@ -1,41 +1,29 @@
 import random
 
 def generator (chislo, ur):
-    if ur == 2:
-        math = ['-', '/', '*', '+']
-        result = []
-        while (chislo > 0):
-            x = random.randint(-150,150)
-            y = random.randint(-150,150)
-            znac = random.choice(math)
-            if x < 0 and y < 0:
-                primer = "(" + str(x) + ")" + znac + "("  + str(y) + ")"
-            if y < 0 and x > 0:
-                primer = str(x) + znac + "(" + str(y) + ")"
-            if y > 0 and x > 0:
-                primer = str(x) + znac + str(y)
-            if y > 0 and x < 0:
-                primer = "(" + str(x) + ")" + znac + str(y)
-            eval(primer)
-            chislo -= 1
-            result.append(primer)
-    if ur == 1:
-        math = ['-', '+']
-        result = []
-        while (chislo > 0):
-            x = random.randint(-50,50)
-            y = random.randint(-50,50)
-            znac = random.choice(math)
-            if x < 0 and y < 0:
-                primer = "(" + str(x) + ")" + znac + "("  + str(y) + ")"
-            if y < 0 and x > 0:
-                primer = str(x) + znac + "(" + str(y) + ")"
-            if y > 0 and x > 0:
-                primer = str(x) + znac + str(y)
-            if y > 0 and x < 0:
-                primer = "(" + str(x) + ")" + znac + str(y)
-            eval(primer)
-            chislo -= 1
-            result.append(primer)
-    return result
+    
+    math = ['-', '//', '*', '+']
+    result = []
 
+    while chislo > 0:
+        qwe = 0
+        x = random.randint(-10,10)
+        primer = str(x)
+        while qwe < ur - 1:
+            
+            znac = random.choice(math)
+            if znac == '*' or '//':
+                x = random.randint(-10,10)
+                if znac == '//' and x == 0:
+                    x = 1
+            else:
+                x = random.randint(-30,30)
+            if x < 0:
+                primer += znac + "(" + str(x) + ")"
+            else:
+                primer += znac + str(x)
+            qwe += 1
+        chislo -= 1
+        
+        result.append(primer)
+    return result
